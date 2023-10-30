1cc58954f09f21c81369244762e485204a853884
thibaudluro <thibaud.luro@gmail.com>
2023-10-30 16:43:47 +0100

    Feat(Footer): Footer added

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -7,37 +7,46 @@ import VerticalTimeline from "../components/common/VerticalTimeline";
 import { topBarItems } from "../utils/constants/TopBarItems";
 
 import { FiChevronsDown } from "react-icons/fi";
+import Footer from "../components/common/Footer";
 
 
 export default function Main() {
 
   return (
-    <div className="h-full w-full p-10 bg-primary font-montserrat">
-      <Topbar topBarItems={topBarItems} />
+    <div className="h-full w-full bg-primary font-montserrat">
+      <div className="p-10">
 
-      <div className=" h-[90vh] w-full flex flex-col items-center justify-center">
-        <MainInfos />
-      </div>
+        <Topbar topBarItems={topBarItems} />
 
-      <div className="flex w-full items-center justify-center bg-primary">
-        <div className="animate-bounce">
-          <FiChevronsDown size={22} className="text-zinc-600" />
+        <div className=" h-[90vh] w-full flex flex-col items-center justify-center">
+          <MainInfos />
         </div>
+
+        <div className="flex w-full items-center justify-center bg-primary">
+          <div className="animate-bounce">
+            <FiChevronsDown size={22} className="text-zinc-600" />
+          </div>
+        </div>
+
+        <div className="pt-20">
+          <h2 className="text-2xl font-bold mb-5">MES REALISATIONS</h2>
+          <MainPageProjects />
+        </div>
+
+        <div className="pt-20">
+          <h1 className="text-2xl font-bold mb-5">A PROPOS</h1>
+          <VerticalTimeline />
+        </div>
+
+        <div className="pt-20">
+          <h2 className="text-2xl font-bold mb-5">CONTACT</h2>
+          <Contact />
+        </div>
+
       </div>
 
       <div className="pt-20">
-        <h2 className="text-2xl font-bold mb-5">MES REALISATIONS</h2>
-        <MainPageProjects />
-      </div>
-
-      <div className="pt-20">
-        <h1 className="text-2xl font-bold mb-5">A PROPOS</h1>
-        <VerticalTimeline />
-      </div>
-
-      <div className="pt-20">
-        <h2 className="text-2xl font-bold mb-5">CONTACT</h2>
-        <Contact />
+        <Footer />
       </div>
 
     </div>
