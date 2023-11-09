a746600c8340b710e1e7cad7f0fb48962129b9f6
thibaudluro <thibaud.luro@gmail.com>
2023-11-09 20:01:00 +0100

    Feat(NotFound): Not found page

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Projects from "./pages/Projects";
 import ProjectDetails from "./pages/ProjectDetails";
 import Skills from "./pages/Skills";
 import SkillDetails from "./pages/SkillDetails";
-// import NotFound from "./pages/NotFound";
+import NotFound from "./pages/NotFound";
 
 export default function App() {
     return (
@@ -15,7 +15,7 @@ export default function App() {
                 <Route path="projects/:name" element={<ProjectDetails />} />
                 <Route path="skills" element={<Skills />} />
                 <Route path="skills/:name" element={<SkillDetails />} />
-                {/* <Route path="*" element={<NotFound />} /> */}
+                <Route path="*" element={<NotFound />} />
             </Routes>
         </BrowserRouter>
     );
--- src/components/common/AppButton.tsx
+++ src/components/common/AppButton.tsx
@@ -1,10 +1,11 @@
 interface IProps {
     placeholder: string;
+    onClick?: () => void;
 }
 
 export default function AppButton(props: IProps) {
     return (
-        <button className="bg-black text-white text-sm py-2 px-6 w-full hover:bg-neutral-800 transition duration-300 ease-in-out">
+        <button onClick={props.onClick} className="bg-black text-white text-sm py-2 px-6 w-full hover:bg-neutral-800 transition duration-300 ease-in-out">
             {props.placeholder}
         </button>
     );
