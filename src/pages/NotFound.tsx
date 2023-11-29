4bef400e9dc6b34d77b3455945e0102768133f40
thibaudluro <thibaud.luro@gmail.com>
2023-11-29 08:22:49 +0100

    Fix(Routes): Portfolio

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -5,7 +5,7 @@ export default function NotFound() {
         <div className="h-[100vh] w-full flex flex-col items-center justify-center bg-primary font-montserrat p-10">
             <h1 className="text-3xl font-bold text-center">CETTE PAGE N'EXISTE PAS</h1>
             <div className="w-44 mt-4">
-                <AppButton placeholder="Retour à l'accueil" onClick={() => window.location.href = "/"}/>
+                <AppButton placeholder="Retour à l'accueil" onClick={() => window.location.href = "/Portfolio/"}/>
             </div>
         </div>
     )
