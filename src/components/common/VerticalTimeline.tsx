c3361ab249c243507b67bc8f1ad000784b24d21e
thibaudluro <thibaud.luro@gmail.com>
2024-02-24 10:28:28 +0100

    Feat(TimeLine): Hover card on items

--- src/components/common/VerticalTimeline.tsx
+++ src/components/common/VerticalTimeline.tsx
@@ -1,4 +1,6 @@
 import { timelineItems } from "../../utils/constants/TimeLineItems";
+import { HoverCard, HoverCardContent, HoverCardTrigger } from "@/components/ui/hover-card";
+import { CalendarIcon } from "@radix-ui/react-icons";
 
 interface IProps {
     index: number;
@@ -8,15 +10,37 @@ interface IProps {
 }
 
 function TimelineItem(props: IProps) {
+
+    const displayedItem =
+        <HoverCard>
+            <HoverCardTrigger className="cursor-pointer">
+                <p className="text-stone-400">{props.date}</p>
+                <h3 className="font-bold mb-1">{props.title}</h3>
+                <p className="text-stone-400">{props.description}</p>
+            </HoverCardTrigger>
+            <HoverCardContent className="w-96 text-start items-start">
+                <div className="flex justify-between space-x-4">
+                    <div className="space-y-1">
+                        <h4 className="text-sm font-semibold">{props.title}</h4>
+                        <p className="text-sm text-stone-400">
+                            {props.description}
+                        </p>
+                        <div className="flex items-center pt-2">
+                            <CalendarIcon className="mr-2 h-4 w-4 opacity-70" />{" "}
+                            <span className="text-xs text-muted-foreground">
+                                {props.date}
+                            </span>
+                        </div>
+                    </div>
+                </div>
+            </HoverCardContent>
+        </HoverCard>
+
     return (
         <div className="flex flex-col w-full md:flex-row items-start justify-center">
             <div className="flex flex-col w-full items-center justify-center pt-4 text-center md:items-end md:text-right md:pt-10">
                 {props.index % 2 !== 0 && (
-                    <>
-                        <p className="text-stone-400">{props.date}</p>
-                        <h3 className="font-bold mb-1">{props.title}</h3>
-                        <p className="text-stone-400">{props.description}</p>
-                    </>
+                    displayedItem
                 )}
             </div>
 
@@ -27,11 +51,7 @@ function TimelineItem(props: IProps) {
 
             <div className="flex flex-col w-full items-center justify-center text-center pb-4 md:items-start md:text-left md:pb-10">
                 {props.index % 2 === 0 && (
-                    <>
-                        <p className="text-stone-400">{props.date}</p>
-                        <h3 className="font-bold mb-1">{props.title}</h3>
-                        <p className="text-stone-400">{props.description}</p>
-                    </>
+                    displayedItem
                 )}
             </div>
         </div>
--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -2,7 +2,7 @@ import VerticalTimeline from "../components/common/VerticalTimeline";
 
 export default function About() {
     return (
-        <div>
+        <div className="h-full w-full">
             <h1 className="text-2xl font-bold mb-5 mt-20">A PROPOS</h1>
             <VerticalTimeline />
         </div>
