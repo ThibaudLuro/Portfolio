ff09580b81e99798824bfeddc06c7ebfa58c79ec
t.luro@intech-sud.fr <t.luro@intech-sud.fr>
2024-03-07 21:30:26 +0100

    Refactor(Skills): Content refactored

--- src/utils/constants/PersonnalPresentation.ts
+++ src/utils/constants/PersonnalPresentation.ts
@@ -4,34 +4,22 @@ export const personnalPresentation: IPersonnalPresentation = {
     explanation: [
         {
             title: "Passion et Professionnalisme",
-            content: `Animé d'une passion pour le développement web fullstack, mon chemin professionnel est articulé autour de valeurs humaines fondamentales telles que la <b>détermination, l'optimisme, et la solidarité</b>. 
-            Sur le court terme mon projet professionnel est animé par une ambition claire : <b>atteindre une maîtrise approfondie des technologies web.</b>
-            Mon but ultime n'est pas seulement d'exceller dans mon domaine, mais également de <b>fonder</b> sur le long terme, <b>une entreprise</b> basée sur un projet qui est en totale corrélation avec moi, marquant ainsi ma contribution à l'innovation et à la création de valeur.`
+            content: `Animé d'une passion pour le développement web fullstack, mon chemin professionnel est basé sur des valeurs humaines telles que la détermination, l'optimisme, et la solidarité. Sur le court terme mon projet professionnel est animé par une ambition claire : Atteindre une maîtrise approfondie des technologies web. Mon but ultime n'est pas seulement d'exceller dans mon domaine, mais également de fonder sur le long terme, une entreprise basée sur un projet qui est en totale corrélation avec moi, marquant ainsi ma contribution à l'innovation et à la création de valeur.`
         },
         {
             title: "Qualités Fondamentales",
-            content: `Mon profil se caractérise par une <b>assiduité, une autonomie, une organisation et une fiabilité</b> remarquables. 
-            Ces attributs sont les piliers sur lesquels je bâtis ma carrière dans le développement web, me permettant d'aborder avec assurance et efficacité les projets variés qui me sont confiés. 
-            Grâce à une rigueur professionnelle et une capacité à envisager des solutions créatives, je suis capable de transformer les défis techniques en réussites concrètes, contribuant ainsi à la <b>réalisation de solutions innovantes et performantes.</b>`
+            content: `Mon profil se caractérise par une assiduité, une autonomie, une organisation et une fiabilité remarquables. Ces attributs sont les piliers sur lesquels je bâtis ma carrière dans le développement web, me permettant d'aborder avec assurance et efficacité les projets variés qui me sont confiés. Grâce à une rigueur professionnelle et une capacité à envisager des solutions créatives, je suis capable de transformer les défis techniques en réussites concrètes, contribuant ainsi à la réalisation de solutions innovantes et performantes.`
         },
         {
             title: "Une Vie Riche d'Expériences",
-            content: `Au-delà des écrans, ma vie est une <b>quête constante de dépassement et d'exploration</b>. 
-            Que ce soit à travers la création d'applications en tout genre (web ou mobile), la création musicale (à travers le logiciel FL Studio), 
-            ou mon engagement dans des activités sportives exigeantes telles que la course à pied, 
-            je me lance dans chaque aventure avec l'ambition de me surpasser et d'élargir mes horizons. 
-            Les voyages et les événements culturels enrichissent mon existence, m'offrant des perspectives nouvelles et stimulant ma créativité. 
-            Ces expériences diversifiées alimentent ma passion pour la découverte et l'innovation, me permettant d'intégrer des influences variées dans mes projets professionnels et personnels.`
+            content: `Au-delà des écrans, ma vie est une quête constante de dépassement et d'exploration. Que ce soit à travers la création d'applications en tout genre (web ou mobile), la création musicale (à travers le logiciel FL Studio), ou mon engagement dans des activités sportives exigeantes telles que la course à pied, je me lance dans chaque aventure avec l'ambition de me surpasser et d'élargir mes horizons. Les voyages et les événements culturels enrichissent mon existence, m'offrant des perspectives nouvelles et stimulant ma créativité. Ces expériences diversifiées alimentent ma passion pour la découverte et l'innovation, me permettant d'intégrer des influences variées dans mes projets professionnels et personnels.`
         },
         {
             title: "Objectifs et Ambitions",
-            content: `Ce portfolio est le miroir de mon parcours professionnel et personnel, mettant en lumière non seulement mon savoir-faire technique, mais aussi les valeurs et les passions qui me définissent. L
-            'objectif est de <b>poursuivre mon évolution dans l'univers du développement web</b>, en privilégiant toujours l'excellence, l'innovation, et une approche humaine. 
-            En parallèle, je prépare le terrain pour <b>concrétiser mon aspiration entrepreneuriale</b>, convaincu que le mariage entre compétences techniques aiguisées et une vision ambitieuse est la clé pour façonner mon avenir.
+            content: `Ce portfolio est la représentation de mon parcours professionnel, mettant en lumière non seulement mon savoir-faire technique, mais aussi les valeurs et les passions qui me définissent. L'objectif est de poursuivre mon évolution dans l'univers du développement web, en privilégiant toujours l'excellence, l'innovation, et une approche humaine. En parallèle, je prépare le terrain pour concrétiser mon aspiration entrepreneuriale, convaincu que le mariage entre compétences techniques aiguisées et une vision ambitieuse est la clé de mon avenir.
             <br/><br/>
-            Mon engagement est de <b>continuer à grandir, à apprendre, et à innover</b>, tout en préparant soigneusement les fondations de mes rêves entrepreneuriaux. 
-            Avec une base solide bâtie sur des projets réussis, une soif d'apprendre, et une volonté de contribuer significativement aux projets dans lesquels je suis impliqué, 
-            je suis prêt à relever les défis futurs et à atteindre pleinement mon potentiel.`
+            Mon engagement est de continuer à grandir, à apprendre, et à innover, tout en préparant soigneusement les fondations de mes rêves entrepreneuriaux. Avec une base solide bâtie sur des projets réussis, une soif d'apprendre, et une volonté de contribuer significativement aux projets dans lesquels je suis impliqué, je suis prêt à relever les défis futurs et à atteindre pleinement mon potentiel.
+            `
         }
     ]
 }
