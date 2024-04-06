fd650facb2994f36e2e26b41d48b24a925051282
thibaudluro <thibaud.luro@gmail.com>
2024-04-06 17:11:21 +0200

    fix: :fire: Not found pageredirection fixed

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,11 +1,14 @@
 import { Button } from "@/components/ui/button";
+import { Link } from "react-router-dom";
 
 export default function NotFound() {
     return (
         <div className="h-[100vh] text-accent w-full flex flex-col items-center justify-center font-montserrat p-10">
             <h1 className="text-3xl font-bold text-center">CETTE PAGE N'EXISTE PAS</h1>
             <div className="w-44 mt-4">
-                <Button onClick={() => window.location.href = "/"}>Retour à l'accueil</Button>
+                <Link to={"/"} style={{ textDecoration: 'none' }}>
+                    <Button>Retour à l'accueil</Button>
+                </Link>
             </div>
         </div>
     )
