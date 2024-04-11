c218cbea8cee66b1adbc6143c9239f0fddc6143b
thibaudluro <thibaud.luro@gmail.com>
2024-04-11 18:22:59 +0200

    fix: :art: gramatical errors and bold words for about section

--- src/utils/constants/PersonnalPresentation.ts
+++ src/utils/constants/PersonnalPresentation.ts
@@ -5,29 +5,29 @@ export const personnalPresentation: IPersonnalPresentation = {
         {
             title: "Passion et Professionnalisme",
             content: `
-            Animé d'une passion pour le développement web fullstack, mon chemin professionnel est basé sur des valeurs humaines telles que la détermination, l'optimisme, et la solidarité. 
+            Animé d'une passion pour le développement web fullstack, mon chemin professionnel est basé sur des valeurs humaines telles que la <b>détermination, l'optimisme, et la solidarité</b>.
             <br/><br/>
-            Ma détermination se manifeste dans ma quête incessante de maîtrise des technologies web, me poussant à surmonter les obstacles et à poursuivre mes objectifs avec acharnement. Après avoir abandonné un projet personnel je me suis juré de ne plus le faire sans raison et je me suis pour l’instant tenu à cette promesse pour trois projets entrepris sur trois.
+            Ma détermination se manifeste dans ma quête incessante de <b>maîtrise des technologies web</b>, me poussant à surmonter les obstacles et à poursuivre mes objectifs avec acharnement. Après avoir abandonné un projet personnel, je me suis juré de ne plus le faire sans raison et je me suis, pour l’instant, tenu à cette promesse pour trois projets entrepris sur trois.
             <br/><br/>
-            L'optimisme, quant à lui, me permet de voir les défis non pas comme des barrières, mais comme des opportunités de croissance. Je pense de cette manière au quotidien que ce soit dans ma carrière professionnelle ou dans ma vie privée.
+            L'optimisme, quant à lui, me permet de voir les défis non pas comme des barrières, mais <b>comme des opportunités</b> de croissance. Je pense de cette manière au quotidien que ce soit dans ma carrière professionnelle ou dans ma vie privée.
             <br/><br/>
-            La solidarité, enfin, se traduit par ma volonté de soutenir et de contribuer au succès collectif, que ce soit par le partage de connaissances ou l'aide aux collègues dans leurs projets. J’ai pour habitude notamment d’aider des camarades de classes sur des sujets techniques à l’approche d’évaluations dans le cadre de mes études. 
+            La solidarité, enfin, se traduit par ma volonté de soutenir et de <b>contribuer au succès collectif</b>, que ce soit par le partage de connaissances ou l'aide aux collègues dans leurs projets. J’ai pour habitude notamment d’aider des camarades de classes sur des sujets techniques à l’approche d’évaluations dans le cadre de mes études.
             <br/><br/>
-            Sur le court terme mon projet professionnel est animé par une ambition claire : atteindre une maîtrise approfondie des technologies web. Mon but ultime n'est pas seulement d'exceller dans mon domaine, mais également de fonder sur le long terme, une entreprise basée sur un projet qui est en totale corrélation avec moi, marquant ainsi ma contribution à l'innovation et à la création de valeur.
+            Sur le court terme, mon projet professionnel est animé par une ambition claire : atteindre une maîtrise approfondie des technologies web. Mon but ultime n'est pas seulement d'exceller dans mon domaine, mais également de fonder sur le long terme une entreprise basée sur un projet qui est en totale corrélation avec moi, marquant ainsi ma contribution à l'innovation et à la création de valeur.
             `
         },
         {
             title: "Qualités Fondamentales",
             content: `
-            Mon profil se caractérise par une assiduité, une autonomie, une organisation et une fiabilité remarquables.
+            Mon profil se caractérise par une <b>assiduité</b>, une <b>autonomie</b>, une <b>organisation</b> et une <b>fiabilité</b> remarquables.
             <br/><br/>
-            L'assiduité se manifeste par ma présence constante, ma constance et mon engagement total dans chaque projet au quotidien, peu importe les événements extérieurs. Je n’ai que des commentaires positifs à ce sujet que ce soit de mes camarades de classe lors de projets de groupe ou de la part de mes supérieurs dans le cadre de mon alternance.
+            L'<b>assiduité</b> se manifeste par ma présence constante, ma constance et mon engagement total dans chaque projet au quotidien, peu importe les événements extérieurs. Je n’ai que des commentaires positifs à ce sujet que ce soit de mes camarades de classe lors de projets de groupe ou de la part de mes supérieurs dans le cadre de mon alternance.
             <br/><br/>
-            Mon autonomie me permet de naviguer avec assurance dans des projets complexes, souvent avec peu de directives. Cette qualité s'est particulièrement affirmée en milieu professionnel, où je suis régulièrement confronté à des défis techniques émergeant des besoins clients, qui se présentent avec peu ou pas d'instructions précises et sans directives techniques m’orientant vers une méthode de réalisation. Cela m'oblige à innover et à trouver des solutions adaptées.
+            Mon <b>autonomie</b> me permet de naviguer avec assurance dans des projets complexes, souvent avec peu de directives. Cette qualité s'est particulièrement affirmée en milieu professionnel, où je suis régulièrement confronté à des défis techniques émergeant des besoins clients, qui se présentent avec peu ou pas d'instructions précises et sans directives techniques m’orientant vers une méthode de réalisation. Cela m'oblige à innover et à trouver des solutions adaptées.
             <br/><br/>
-            Mon sens de l'organisation est crucial pour gérer simultanément divers aspects des projets, assurant ainsi une progression fluide et cohérente. En entreprise, je me fixe quotidiennement des objectifs classés par priorités en plus d’imaginer pour mes projets personnels et scolaires chaque semaine une liste de tâche réparties entre les différents jours de celle-ci. Cela me permet d’avoir une vision claire et globale tout en avançant sereinement dans mes objectifs.
+            Mon sens de l'<b>organisation</b> est crucial pour gérer simultanément divers aspects des projets, assurant ainsi une progression fluide et cohérente. En entreprise, je me fixe quotidiennement des objectifs classés par priorités en plus d’imaginer pour mes projets personnels et scolaires chaque semaine une liste de tâche réparties entre les différents jours de celle-ci. Cela me permet d’avoir une vision claire et globale tout en avançant sereinement dans mes objectifs.
             <br/><br/>
-            La fiabilité, enfin, est le fondement de la confiance que mes collègues et supérieurs placent en moi, sachant que je livre constamment un travail de qualité dans les délais impartis. C’est quelque chose d’important pour moi qui est étroitement lié à mon assiduité et constance.
+            La <b>fiabilité</b>, enfin, est le fondement de la confiance que mes collègues et supérieurs placent en moi, sachant que je livre constamment un travail de qualité dans les délais impartis. C’est quelque chose d’important pour moi qui est étroitement lié à mon assiduité et constance.
             <br/><br/>
             Ces attributs sont les piliers sur lesquels je bâtis ma carrière dans le développement web, me permettant d'aborder avec assurance et efficacité les projets variés qui me sont confiés. Grâce à une rigueur professionnelle, je suis capable de transformer les défis techniques en réussites concrètes, contribuant ainsi à la réalisation de solutions innovantes et performantes.
             `
@@ -35,13 +35,13 @@ export const personnalPresentation: IPersonnalPresentation = {
         {
             title: "Centres d’intérêts",
             content: `
-            Au-delà des écrans, ma vie est une quête constante de dépassement et d'exploration.
+            Au-delà des écrans, ma vie est une quête constante de <b>dépassement et d'exploration</b>.
             <br/><br/>
-            Que ce soit à travers mes centres d’intérêts comme la création musicale (à travers le logiciel de production FL Studio) à des fins personnelles d’apprentissage et de passion ou encore l’écoute ou la diffusion de celle-ci à travers des contrôleurs DJ pour des soirées entre amis. La musique représente une partie importante de ma vie, c’est l’un des arts me procurant le plus d’émotion et d’engouement.
+            Que ce soit à travers mes centres d’intérêts comme la <b>création musicale</b> (à travers le logiciel de production FL Studio, ou l'apprentissage du solfège) à des fins personnelles d’apprentissage et de passion, ou encore l’écoute ou la diffusion de celle-ci à travers des contrôleurs DJ pour des soirées entre amis. La musique représente une partie importante de ma vie, c’est l’un des arts me procurant le plus d’émotion et d’engouement.
             <br/><br/>
-            Je pratique également des activités sportives telles que la course à pied ou la musculation tout en étant passé par l’athlétisme ou la boxe anglaise, je me lance dans chaque aventure avec l'ambition de me surpasser et d'élargir mes horizons. 
+            Je pratique également des <b>activités sportives</b> telles que la course à pied ou la musculation tout en étant passé par l’athlétisme ou la boxe anglaise. Je me lance dans chaque aventure avec l'ambition de me surpasser et d'élargir mes horizons.
             <br/><br/>
-            Les voyages et les événements culturels sont également des sujets qui me passionnent. Je n’ai commencé ce genre d’activités que récemment mais je souhaite les multiplier afin de vivre de nouvelles expériences, sortir de ma zone de comfort et découvrir des cultures, c’est également quelque chose stimulant ma créativité. 
+            Les <b>voyages et les événements culturels</b> sont également des sujets qui me passionnent. Je n’ai commencé ce genre d’activités que récemment mais je souhaite les multiplier afin de vivre de nouvelles expériences, sortir de ma zone de confort et découvrir des cultures. C’est également quelque chose stimulant ma créativité.
             <br/><br/>
             Ces expériences diversifiées alimentent ma passion pour la découverte et l'innovation, me permettant d'intégrer des influences variées dans mes projets professionnels et personnels.
             `
@@ -51,7 +51,7 @@ export const personnalPresentation: IPersonnalPresentation = {
             content: `
             Ce portfolio est la représentation de mon parcours professionnel, mettant en lumière non seulement mon savoir-faire technique, mais aussi les valeurs et les passions qui me définissent. L'objectif est de poursuivre mon évolution dans l'univers du développement web, en privilégiant toujours l'excellence, l'innovation, et une approche humaine. En parallèle, je prépare le terrain pour concrétiser mon aspiration entrepreneuriale, convaincu que le mariage entre compétences techniques aiguisées et une vision ambitieuse est la clé de mon avenir.
             <br/><br/>
-            Mon engagement est de continuer à grandir, à apprendre, et à innover, tout en préparant soigneusement les fondations de mes rêves entrepreneuriaux. Avec une base solide bâtie sur des projets réussis, une soif d'apprendre, et une volonté de contribuer significativement aux projets dans lesquels je suis impliqué, je suis prêt à relever les défis futurs et à atteindre pleinement mon potentiel.
+            Mon engagement est de <b>continuer à grandir, à apprendre, et à innover</b>, tout en préparant soigneusement les fondations de mes rêves entrepreneuriaux. Avec une base solide bâtie sur des projets réussis, une soif d'apprendre, et une volonté de contribuer significativement aux projets dans lesquels je suis impliqué, je suis prêt à relever les défis futurs et à atteindre pleinement mon potentiel.
             `
         }
     ]
--- src/utils/constants/TimeLineItems.ts
+++ src/utils/constants/TimeLineItems.ts
@@ -8,13 +8,13 @@ export const timelineItems: ITimeLineItem[] = [
         title: "ATLERNANCE CHEZ HOLIS CONSULTING",
         shortDescription: "Alternant pendant 2 ans en tant que développeur web full stack",
         longDescription: `
-            Durant mon alternance j'ai exclusivement travaillé sur le projet PREMIS DM.
-            <br/><br/>
-            J'ai eu la responsabilité de concevoir et de développer de 0 cette application web destinée la vente.
-            <br/><br/>
-            L'application est un outil de gestion de données issues de l'industrie, elle permet de gérer les données des entreprises de ce secteur, de les analyser et de les visualiser.
-            <br/><br/>
-            J'ai travaillé en étroite collaboration avec les ingénieurs de l'entreprise pour concevoir une application qui réponde aux besoins et objectifs définis au lancement du développement.
+        Durant mon alternance, j'ai exclusivement travaillé sur le projet PREMIS DM.
+        <br/><br/>
+        J'ai eu la responsabilité de <b>concevoir et de développer</b> de 0 cette application web destinée à la vente.
+        <br/><br/>
+        L'application est un outil de gestion de données issues de l'industrie, elle permet de gérer les données des entreprises de ce secteur, de les <b>analyser et de les visualiser</b>.
+        <br/><br/>
+        J'ai travaillé en étroite collaboration avec les ingénieurs de l'entreprise pour concevoir une application qui réponde aux besoins et objectifs définis au lancement du développement.
         `,
         projectsLinked: ["PREMIS DM"],
         skillsLinked: ["React", "Express JS", "Base de données", "Docker"]
@@ -26,13 +26,13 @@ export const timelineItems: ITimeLineItem[] = [
         title: "STAGE CHEZ HOLIS CONSULTING",
         shortDescription: "Stagiaire durant 6 mois en tant que développeur web full stack",
         longDescription: `
-            Durant mon stage, j'ai directement eu la responsabilité d'une application web destinée à l'usage interne des ingénieurs de l'entreprise. 
-            <br/><br/>
-            Mon positionnement dans l'entreprise s'est caractérisé par un fort degré d'autonomie, je récupérais les besoins clients moi même et les transformaient en fonctionnalités au sein de l'application web. 
-            <br/><br/>
-            Cette expérience m' a permis de gagner la confiance des mes supérieurs qui m'ont donné la responsabilité de conceptualiser et de développer une application web de 0, destiné à la vente pour de futurs clients. 
-            <br/><br/>
-            J'ai directement obtenu des responsabilités au sein de l'entreprise.
+        Durant mon stage, j'ai directement travailé sur une <b>application web</b> destinée à l'usage interne des ingénieurs de l'entreprise.
+        <br/><br/>
+        Mon positionnement dans l'entreprise s'est caractérisé par un fort degré d'<b>autonomie</b>, je récupérais les besoins clients moi-même et les transformais en fonctionnalités au sein de l'application web.
+        <br/><br/>
+        Cette expérience m'a permis de gagner la confiance de mes supérieurs qui m'ont donné pour mission de <b>conceptualiser et de développer</b> une application web de 0, destinée à la vente pour de futurs clients.
+        <br/><br/>
+        J'ai directement obtenu des <b>responsabilités</b> au sein de l'entreprise.
         `,
         projectsLinked: ["ISIS", "PREMIS DM"],
         skillsLinked: ["React", "Express JS", "Gestion de Projet Agile et Scrum", "Base de données", "Docker"]
@@ -44,15 +44,15 @@ export const timelineItems: ITimeLineItem[] = [
         title: "ESIEA",
         shortDescription: "Expert en ingénierie du logiciel",
         longDescription: `
-            J'ai suivi un cursus d'Expert en ingénierie du logiciel (Bac +5, RNCP35419 niveau 7). 
-            <br/><br/>
-            Celui-ci a la particularité d'avoir sa pédagogie par projets. Chaque semestre, nous en réalisions deux (un informatique et un de formation humaine). 
-            <br/><br/>
-            Les deux sont des projets de groupe, en relation avec des entreprises ou des associations proches de l'école. 
-            <br/><br/>
-            Les projets informatique nous forment à traiter des besoins réels et concrets. 
-            <br/><br/>
-            Les projets de formation humaine nous forment aux compétences transverses, aussi appelées soft skills.
+        J'ai suivi un cursus d'<b>Expert en ingénierie du logiciel</b> (Bac +5, RNCP35419 niveau 7).
+        <br/><br/>
+        Celui-ci a la particularité d'avoir sa pédagogie par projets. Chaque semestre, nous en réalisions deux (un informatique et un de formation humaine).
+        <br/><br/>
+        Les deux sont des <b>projets de groupe</b>, en relation avec des entreprises ou des associations proches de l'école.
+        <br/><br/>
+        Les projets informatique nous forment à traiter des <b>besoins réels et concrets</b>.
+        <br/><br/>
+        Les projets de formation humaine nous forment aux compétences transverses, aussi appelées <b>soft skills</b>.
         `
     },
     {
@@ -62,11 +62,11 @@ export const timelineItems: ITimeLineItem[] = [
         title: "LYCÉE MAURICE RAVEL",
         shortDescription: "Bacaclauréat Scientifique option Sciences de la Vie et de la Terre spécialité Mathématiques",
         longDescription: `
-            J'ai effectué mon second degré au lycée Maurice Ravel, à Saint jean de luz. 
-            <br/><br/>
-            A l'époque, le lycée proposait plusieurs baccalauréats, comme le bac S, le bac ES et le bac STMG. 
-            <br/><br/>
-            Mon attrait pour les sciences et plus particulièrement pour les mathématiques m'a poussé à choisir le bac S avec l'option SVT qui était la seule disponible et la spécialité Mathématiques.
+        J'ai effectué mon second degré au lycée <b>Maurice Ravel</b>, à <b>Saint-Jean-de-Luz</b>.
+        <br/><br/>
+        À l'époque, le lycée proposait plusieurs baccalauréats, comme le bac S, le bac ES et le bac STMG.
+        <br/><br/>
+        Mon attrait pour les sciences et plus particulièrement pour les mathématiques m'a poussé à choisir le bac S avec l'option SVT, qui était la seule disponible, et la spécialité <b>Mathématiques</b>.
             `
     },
 ];
