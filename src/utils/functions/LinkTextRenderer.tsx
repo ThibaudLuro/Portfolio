3f9cf007a62f8d3286b0cb73620d137100900f00
t.luro@intech-sud.fr <t.luro@intech-sud.fr>
2024-04-16 21:21:33 +0200

    feat: :dizzy: Redirections for projects to skills in paragraphs

--- src/utils/functions/LinkTextRenderer.tsx
+++ src/utils/functions/LinkTextRenderer.tsx
@@ -2,12 +2,14 @@ import { Link } from "react-router-dom";
 
 function LinkTextRenderer(text: string) {
     const segments = text.split(/<\/?a>/);
+    const url = window.location.href;
+    const isSkills = url.includes('skills');
 
     return (
         segments.map((segment, index) => {
             const isLink = index % 2 === 1;
             return isLink ? (
-                <Link to={`/projects/${segment.trim()}`} key={index} style={{ textDecoration: 'none' }}>
+                <Link to={`/${isSkills ? "projects" : "skills" }/${segment.trim()}`} key={index} style={{ textDecoration: 'none' }}>
                     <span key={index} className="text-brown font-semibold cursor-pointer">
                         {segment}
                     </span>
