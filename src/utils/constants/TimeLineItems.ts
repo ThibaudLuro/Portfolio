53dc69237dc9ac09583b1df284eed144828915de
t.luro@intech-sud.fr <t.luro@intech-sud.fr>
2024-07-27 18:34:00 +0200

    refactor: rephrase

--- src/utils/constants/TimeLineItems.ts
+++ src/utils/constants/TimeLineItems.ts
@@ -8,13 +8,13 @@ export const timelineItems: ITimeLineItem[] = [
         title: "ATLERNANCE CHEZ HOLIS CONSULTING",
         shortDescription: "Alternant pendant 2 ans en tant que développeur web full stack",
         longDescription: `
-        Durant mon alternance, j'ai exclusivement travaillé sur le projet PREMIS DM.
+        Durant mon alternance j'ai exclusivement travaillé sur le projet PREMIS DM.
         <br/><br/>
         J'ai eu la responsabilité de <b>concevoir et de développer</b> de 0 cette application web destinée à la vente.
         <br/><br/>
         L'application est un outil de gestion de données issues de l'industrie, elle permet de gérer les données des entreprises de ce secteur, de les <b>analyser et de les visualiser</b>.
         <br/><br/>
-        J'ai travaillé en étroite collaboration avec les ingénieurs de l'entreprise pour concevoir une application qui réponde aux besoins et objectifs définis au lancement du développement.
+        J'ai travaillé en collaboration avec les ingénieurs de l'entreprise pour concevoir une application qui réponde aux besoins et objectifs définis au lancement du développement.
         `,
         projectsLinked: ["PREMIS DM"],
         skillsLinked: ["React", "Express JS", "Base de données", "Docker"]
@@ -26,11 +26,11 @@ export const timelineItems: ITimeLineItem[] = [
         title: "STAGE CHEZ HOLIS CONSULTING",
         shortDescription: "Stagiaire durant 6 mois en tant que développeur web full stack",
         longDescription: `
-        Durant mon stage, j'ai directement travailé sur une <b>application web</b> destinée à l'usage interne des ingénieurs de l'entreprise.
+        Durant mon stage, j'ai directement travailé sur une <b>application web</b> destinée à un usage interne des ingénieurs de l'entreprise.
         <br/><br/>
-        Mon positionnement dans l'entreprise s'est caractérisé par un fort degré d'<b>autonomie</b>, je récupérais les besoins clients moi-même et les transformais en fonctionnalités au sein de l'application web.
+        J'ai directement appris à travailler en <b>autonomie</b>, je récupérais les besoins clients moi-même et les transformais en fonctionnalités au sein de l'application web.
         <br/><br/>
-        Cette expérience m'a permis de gagner la confiance de mes supérieurs qui m'ont donné pour mission de <b>conceptualiser et de développer</b> une application web de 0, destinée à la vente pour de futurs clients.
+        C'est cette expérience qui m'a permis de gagner la confiance de mes supérieurs qui m'ont donné pour mission de <b>conceptualiser et de développer</b> une application web de 0, destinée à la vente pour de futurs clients.
         <br/><br/>
         J'ai directement obtenu des <b>responsabilités</b> au sein de l'entreprise.
         `,
